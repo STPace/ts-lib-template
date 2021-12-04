d2e84a9a6c743f9e840e16608aaca02c438ec31d
STPace <heliapan@outlook.com>
2021-12-05 01:57:55 +0800

    fix: remove package-lock

--- release.config.js
+++ release.config.js
@@ -20,7 +20,7 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        assets: ['CHANGELOG.md', 'package.json'],
         message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }
     ]
